c4f4dc8044daeb1166297ad978211dd08b576485
MaauVe <brandon.vegar@alumno.buap.mx>
2023-09-27 22:37:20 -0600

    Se agrega el Proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+//Componentes
+import { LoginScreenComponent } from './screens/login-screen/login-screen.component';
+import { RegistroScreenComponent } from './screens/registro-screen/registro-screen.component';
+import { HomeScreenComponent } from './screens/home-screen/home-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Angular material
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+//Cambia el idioma a español
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+//Mask
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+import { NuevoLoginScreenComponent } from './screens/nuevo-login-screen/nuevo-login-screen.component';
+import { RegistroProductoScreenComponent } from './screens/registro-producto-screen/registro-producto-screen.component';
+//Options mask
+export const options: Partial<IConfig> | (() => Partial<IConfig>) = {};
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginScreenComponent,
+    RegistroScreenComponent,
+    HomeScreenComponent,
+    NuevoLoginScreenComponent,
+    RegistroProductoScreenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    NgxMaskModule.forRoot(options),
+  ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'es-ES'},
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
